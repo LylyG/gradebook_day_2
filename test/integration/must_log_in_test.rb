72738f5f2671805233412d159e43d71e45f4a2c9
LylyG <lgalarza@spoonflower.com>
2016-03-10 02:42:44 -0500

    Add more steps into integration test.

--- test/integration/must_log_in_test.rb
+++ test/integration/must_log_in_test.rb
@@ -19,11 +19,21 @@ class MustLogInTest < ActionDispatch::IntegrationTest
     assert_select "tbody tr", Teacher.count
 
     #Create a teacher
+    get new_teacher_path name: "Dana", email: "Dana@dana.com", password: "password"
+    post teachers_path
 
     #Make sure that I see one more.
+    assert_select "tbody tr", Teacher.count
 
     # Log out
+    get logout_path
+    assert_redirected_to login_path
+    follow_redirect!
+    assert_equal nil, session[:user_id]
+    assert_equal nil, session[:user_type]
+    assert_template "new"
 
     # Make sure I go to the login page.
+
   end
 end
